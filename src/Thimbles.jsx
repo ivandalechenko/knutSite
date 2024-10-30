712c5ce0d276d8981fdbb677ff2a6b33cedc6651
ivandalechenko <ivandalechenko@gmail.com>
2024-10-30 21:58:34 +0200

    added afk thimbles

--- src/Thimbles.jsx
+++ src/Thimbles.jsx
@@ -3,17 +3,28 @@ import './scss/Thimbles.scss';
 import Window from './Window';
 
 import ThimblesBid from './ThimblesBid'
-import ThimblesGame from './ThimblesGame'
 import ThimblesLastGames from './ThimblesLastGames'
+import ThimblesGame from './ThimblesGame'
+import ThimblesGameOffline from './ThimblesGameOffline'
 import { observer } from 'mobx-react-lite';
 
 const Thimbles = () => {
     return (
         <Window type='thimbles'>
             <div className='Thimbles'>
-                <ThimblesGame />
-                <ThimblesBid />
-                <ThimblesLastGames />
+                <ThimblesGameOffline />
+                <div className='Thimbles_soon window'>
+                    <div className='Thimbles_soon_bid'>
+                        <input type="text" value={10} />
+                        <button>Place bid</button>
+                    </div>
+                    <div className='Thimbles_soon_text'>
+                        Cumming soon...
+                    </div>
+                </div>
+                {/* <ThimblesGame /> */}
+                {/* <ThimblesBid /> */}
+                {/* <ThimblesLastGames /> */}
             </div>
         </Window>
     );
