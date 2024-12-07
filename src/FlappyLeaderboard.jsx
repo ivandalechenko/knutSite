e74849d41145d5ed3b6de069f457f7a251739e83
ivandalechenko <ivandalechenko@gmail.com>
2024-12-07 23:53:57 +0200

    fix

--- src/FlappyLeaderboard.jsx
+++ src/FlappyLeaderboard.jsx
@@ -21,27 +21,31 @@ const FlappyLeaderboard = ({ type }) => {
     return (
         <Window type={`${type}Leaderboard`} >
             <div className='FlappyLeaderboard'>
-                {lb.map((lbel, index) => (
-                    <div className='FlappyLeaderboard_element window' key={`lbel-${lbel._id}`}>
-                        <div className='FlappyLeaderboard_element_name'>
-                            <div className='FlappyLeaderboard_element_decor'>
-                                <img src={`/img/flappy/${index < 3 ? `cup${index + 1}` : 'star'}.png`} alt='decor' />
-                                <img src='/img/flappy/coin.png' alt='decor' />
+                {lb.map((lbel, index) => {
+                    if (lbel.wallet) {
+                        return <div className='FlappyLeaderboard_element window' key={`lbel-${lbel._id}`}>
+                            <div className='FlappyLeaderboard_element_name'>
+                                <div className='FlappyLeaderboard_element_decor'>
+                                    <img src={`/img/flappy/${index < 3 ? `cup${index + 1}` : 'star'}.png`} alt='decor' />
+                                    <img src='/img/flappy/coin.png' alt='decor' />
+                                </div>
+                                <div className='FlappyLeaderboard_element_info'>
+                                    <div className='FlappyLeaderboard_element_info_value'>{lbel.wallet.slice(0, 4)}...{lbel.wallet.slice(-4)}</div>
+                                </div>
                             </div>
-                            <div className='FlappyLeaderboard_element_info'>
-                                <div className='FlappyLeaderboard_element_info_value'>{lbel.wallet.slice(0, 4)}...{lbel.wallet.slice(-4)}</div>
-                            </div>
-                        </div>
-                        <div className='FlappyLeaderboard_element_right'>
-                            {index + 1}{index + 1 === 1 ? 'st' : index + 1 === 2 ? 'nd' : 'th'}
-                            <div className='FlappyLeaderboard_element_value_wrapper'>
-                                <div className='FlappyLeaderboard_element_value'>
-                                    {lbel.score}
+                            <div className='FlappyLeaderboard_element_right'>
+                                {index + 1}{index + 1 === 1 ? 'st' : index + 1 === 2 ? 'nd' : 'th'}
+                                <div className='FlappyLeaderboard_element_value_wrapper'>
+                                    <div className='FlappyLeaderboard_element_value'>
+                                        {lbel.score}
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    }
+                }
+
+                )}
             </div>
         </Window>
     )
